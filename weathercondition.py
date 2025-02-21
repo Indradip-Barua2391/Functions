dca4faf179d4d082731835562a864d1a5d888f33
Indradip <chadsigmarizzler@gmail.com>
2025-02-21 16:14:11 +0600

    first commit

---
+++ weathercondition.py
@@ -0,0 +1,12 @@
+def weatherCondition(s1,s2):
+    print(f"The Season Is Very Cold And Cozy In {s1}")
+    print(f"The Season Is nice In {s2}")
+    
+season1 = "Winter"
+season2= "Spring"
+
+weatherCondition(season1, season2)
+
+print()
+
+weatherCondition(season2, season1)
---
+++ well wishes.py
@@ -0,0 +1,11 @@
+def wishes(n):
+    print(f"Hello Good Day Sir {n}! ")
+    
+name = "rajmoyukh"
+
+
+wishes(name)
+wishes(name)
+wishes(name)
+wishes(name)
+wishes(name)
