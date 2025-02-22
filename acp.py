6409f7b1cca57c9d1a4e0aeb9e02d38fd1162cc1
Indradip <chadsigmarizzler@gmail.com>
2025-02-22 17:23:15 +0600

    abc

---
+++ acp.py
@@ -0,0 +1,16 @@
+def circumference(r):
+    return 2 * 3.14159 * r
+
+
+def area(r):
+    return 3.14159 * (r**2)
+
+
+radius = float(input("Enter the radius: "))
+
+result1 = circumference(radius)
+result2 = area(radius)
+
+
+print(f"the circumference of the circle is = {resul1}")
+print(f"the area of the circle is = {resul1}")
